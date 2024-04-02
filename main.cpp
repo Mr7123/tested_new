277204c10c3f7394f16b3b5eddfe1841cbf4d799
Mr712 <DZGROK@gmail.com>
2024-04-02 10:59:20 +0800

    Добавлен вывод суммы

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
-main()
+using System;
+
+namespace _02._04._24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int _A = 1;
+            int _B = 4;
+            int _Summa = _A + _B;
+            Console.WriteLine("Сумма введённых чисел равна: ");
+            Console.Write(_Summa);
+        }
+    }
+}
