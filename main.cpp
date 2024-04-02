6a6a3b4db95b1dd542d4b8aae949238f6ad718cb
Mr712 <DZGROK@gmail.com>
2024-04-02 11:02:55 +0800

    Добавлено числа с приглашениями

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ namespace _02._04._24
     {
         static void Main(string[] args)
         {
-            int _A = 1;
-            int _B = 4;
+            int _A = Convert.ToInt32(Console.ReadLine());
+            int _B = Convert.ToInt32(Console.ReadLine());
             int _Summa = _A + _B;
             Console.WriteLine("Сумма введённых чисел равна: ");
             Console.Write(_Summa);
